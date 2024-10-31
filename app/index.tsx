7938500ec1b6f87e817030c4bc7da05d994fe21b
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-31 10:36:54 -0300

    feat: created the initial page

---
+++ app/index.tsx
@@ -0,0 +1,47 @@
+import { globalStyles } from "@/styles/globalStyles";
+import { Image, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Button from "@/components/Button";
+import { colors } from "@/constraints/color";
+import { useRouter } from "expo-router";
+
+export default function InitialPage() {
+  const router = useRouter();
+
+  return (
+    <LinearGradient
+      colors={["#141518", "#221C3D"]} // Cores do gradiente
+      start={{ x: 0.2, y: 0 }} // Define o ponto inicial do gradiente
+      end={{ x: 0.9, y: 1 }} // Define o ponto final do gradiente
+      style={[globalStyles.centralization, { gap: 52 }]}
+    >
+      <View>
+        <Image source={require("@/assets/Logo.png")} />
+      </View>
+      <View style={{ gap: 24 }}>
+        <Button onPress={() => router.push("/login")}>
+          <Text
+            style={{
+              color: colors.textPrimary,
+              fontWeight: "600",
+              fontSize: 16,
+            }}
+          >
+            Login
+          </Text>
+        </Button>
+        <Button>
+          <Text
+            style={{
+              color: colors.textPrimary,
+              fontWeight: "600",
+              fontSize: 16,
+            }}
+          >
+            Sign Up
+          </Text>
+        </Button>
+      </View>
+    </LinearGradient>
+  );
+}
