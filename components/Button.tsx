2a9b416072acec0108a963edf00728a2b0192eec
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-31 10:37:37 -0300

    feat: created the button component

---
+++ components/Button.tsx
@@ -0,0 +1,38 @@
+import { colors } from "@/constraints/color";
+import React from "react";
+import {
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+} from "react-native";
+
+interface ButtonProps {
+  style?: StyleProp<TextStyle>;
+  onPress?: () => void;
+  color?: string; // Cor opcional para o botão
+  children?: React.ReactNode;
+}
+
+export default function Button(props: ButtonProps) {
+  return (
+    <Pressable
+      style={({ pressed }) => [{ opacity: pressed ? 0.6 : 1 }, styles.button]}
+      onPress={props.onPress}
+    >
+      {props.children}
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: colors.primaryPurple,
+    paddingHorizontal: 116,
+    paddingVertical: 15,
+    borderRadius: 10,
+    width: 360,
+  },
+});
