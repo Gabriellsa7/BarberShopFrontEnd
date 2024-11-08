fcfc568363e282480aa8cabb34e75f0666368e35
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-11-08 20:46:48 -0300

    feat: created the colors

---
+++ constraints/color.ts
@@ -0,0 +1,28 @@
+export const colors = {
+  // Main Colors
+  darkPurple: "#221C3D",
+  primaryPurple: "#8162FF",
+  secondary: "#6c757d",
+  accent: "#ffc107",
+
+  // Background Colors
+  backgroundBlack: "#141518",
+  secondaryBlack: "#1A1B1F",
+
+  // Text
+  textPrimary: "#FFFFFF",
+  textSecondary: "#838896",
+
+  // Status
+  success: "#28a745",
+  error: "#dc3545",
+  warning: "#ffc107",
+  info: "#17a2b8",
+
+  // Neutral
+  white: "#ffffff",
+  black: "#000000",
+  lightGray: "#f1f1f1",
+  mediumGray: "#6c757d",
+  darkGray: "#343a40",
+};
